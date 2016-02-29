eb3c9230f7e030b0e308a012146a56e16aee09d8
Christian Weyer <christian.weyer@thinktecture.com>
2016-02-29 16:06:54 +0100

    First Web API version

---
+++ source/server/BASTAWebApi/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace BASTAWebApi
+{
+    public class ProductsController : ApiController
+    {
+        [HttpGet]
+        public string Ping()
+        {
+            return "Pong";
+        }
+
+        [HttpGet]
+        public List<Product> List()
+        {
+            // TODO: Talk to DB :)
+
+            var list = new List<Product>
+            {
+                new Product {Id = 1, Name = "Foo", Description = "Bar"},
+                new Product {Id = 2, Name = "Bar", Description = "Baz"}
+            };
+
+            return list;
+        } 
+    }
+}
---
+++ source/server/BASTAWebApi/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Newtonsoft.Json.Serialization;
+using Owin;
+
+namespace BASTAWebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //app.UseWelcomePage();
+
+            var httpCOnfig = new HttpConfiguration();
+
+            httpCOnfig.Routes.MapHttpRoute("DefaultApi",
+                "api/{controller}/{action}");
+
+            httpCOnfig.Formatters.Clear();
+
+            var jsonFormatter = new JsonMediaTypeFormatter();
+            jsonFormatter.SerializerSettings.ContractResolver = 
+                new CamelCasePropertyNamesContractResolver();
+            httpCOnfig.Formatters.Add(jsonFormatter);
+            
+            httpCOnfig.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+
+            app.UseWebApi(httpCOnfig);
+        }
+    }
+}
